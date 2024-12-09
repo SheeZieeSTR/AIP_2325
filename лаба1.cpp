73880f0d92165c24a58e9117376fb447a272aa3c
SheeZieeSTR <strukoff.alex@gmail.com>
2024-12-09 11:43:25 +0300

    Create лаба1.cpp

---
+++ лаба1.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+#include <cmath>
+#include <stdexcept>
+
+using namespace std;
+
+class CustomException : public exception {
+private:
+    string errorMessage;
+    double errorValue;
+    int errorCode;
+
+public:
+    CustomException(const string& message, double value, int code)
+        : errorMessage(message), errorValue(value), errorCode(code) {}
+
+    const char* what() const noexcept override {
+        return errorMessage.c_str();
+    }
+
+    double getErrorValue() const {
+        return errorValue;
+    }
+
+    int getErrorCode() const {
+        return errorCode;
+    }
+};
+
+double calculateZ1(double& a) {
+
+    if (a == 0) throw CustomException("Деление на 0 Z1", a, 1);
+
+    if (a - sqrt(2 * a) == 0) throw CustomException("Деление на 0 Z1", a, 1);
+
+    double z1 = (sqrt(2 + sqrt((a * a) - 4)) + 2 * a) / (a + sqrt((a * a) - 4) + 2);
+
+
+
+    if (isnan(z1)) {
+        throw CustomException("NaN Z1", z1, 3);
+    }
+
+    return z1;
+}
+
+double calculateZ2(double& a) {
+    if (a + sqrt(2) == 0) throw CustomException("Деление на 0 Z2", a, 2);
+
+    double z2 = (1) / (sqrt(a + 2));
+
+    if (isnan(z2)) {
+        throw CustomException("NaN Z2", z2, 3);
+    }
+
+    return z2;
+}
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    double a;
+
+    cout << "Введите значение a: ";
+    cin >> a;
+
+    try {
+        double z1 = calculateZ1(a);
+        double z2 = calculateZ2(a);
+
+        cout << "Z1 = " << z1 << endl;
+        cout << "Z2 = " << z2 << endl;
+
+        if (z1 == z2) {
+            cout << "Значения Z1 и Z2 приблизительно равны." << endl;
+        }
+        else {
+            cout << "Значения Z1 и Z2 различаются." << endl;
+        }
+
+    }
+    catch (const CustomException& ex) {
+        cerr << "Произошла ошибка: " << ex.what() << endl;
+        cerr << "Значение, вызвавшее ошибку: " << ex.getErrorValue() << endl;
+        cerr << "Код ошибки: " << ex.getErrorCode() << endl;
+    }
+
+    return 0;
+}
