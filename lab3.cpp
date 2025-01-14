673342f0ae3c338b8a91a70b618dc8890fdb9af9
HayaSH1111 <glebikgleb6@gmail.com>
2025-01-14 13:46:45 +0300

    Update lab3.cpp

--- lab3.cpp
+++ lab3.cpp
@@ -5,7 +5,6 @@
 #include <string>
 #include <iterator>
 
-// Custom exception class
 class CustomException : public std::exception {
 private:
     std::string message;
@@ -22,7 +21,6 @@ public:
     int getErrorCode() const { return error_code; }
 };
 
-// Node structure
 template <typename T>
 struct Node {
     T value;
@@ -32,7 +30,6 @@ struct Node {
     Node(const T& val) : value(val), next(nullptr), prev() {}
 };
 
-// Template List class
 template <typename T>
 class List {
 private:
@@ -41,30 +38,26 @@ private:
     size_t size;
 
 public:
-    // Default constructor
+    
     List() : head(nullptr), tail(nullptr), size(0) {}
 
-    // Constructor from initializer list
     List(std::initializer_list<T> init_list) : List() {
         for (const T& value : init_list) {
             push_back(value);
         }
     }
 
-    // Copy constructor
     List(const List<T>& other) : List() {
         for (auto node = other.head; node != nullptr; node = node->next) {
             push_back(node->value);
         }
     }
 
-    // Move constructor
     List(List<T>&& other) noexcept
         : head(std::move(other.head)), tail(std::move(other.tail)), size(other.size) {
         other.size = 0;
     }
 
-    // Copy assignment operator
     List& operator=(const List<T>& other) {
         if (this != &other) {
             clear();
@@ -75,7 +68,6 @@ public:
         return *this;
     }
 
-    // Move assignment operator
     List& operator=(List<T>&& other) noexcept {
         if (this != &other) {
             clear();
@@ -87,12 +79,10 @@ public:
         return *this;
     }
 
-    // Destructor
     ~List() {
         clear();
     }
 
-    // Push back
     void push_back(const T& value) {
         auto new_node = std::make_shared<Node<T>>(value);
         if (tail) {
@@ -105,7 +95,6 @@ public:
         ++size;
     }
 
-    // Pop back
     void pop_back() {
         if (!tail) {
             throw CustomException("Cannot pop from an empty list", 101);
@@ -120,12 +109,10 @@ public:
         --size;
     }
 
-    // Size
     size_t get_size() const {
         return size;
     }
 
-    // Clear list
     void clear() {
         while (head) {
             head = head->next;
@@ -134,7 +121,6 @@ public:
         size = 0;
     }
 
-    // Print list
     void print() const {
         for (auto node = head; node != nullptr; node = node->next) {
             std::cout << node->value << " ";
@@ -143,7 +129,6 @@ public:
     }
 };
 
-// Main function to test the List class
 template <typename T>
 void test_list() {
     try {
