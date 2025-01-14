acc2e2f6defb656ce27c469ac67ea04f1be7f728
HayaSH1111 <glebikgleb6@gmail.com>
2025-01-14 13:47:33 +0300

    Update lap4.cpp

--- lap4.cpp
+++ lap4.cpp
@@ -2,19 +2,17 @@
 #include <iomanip>
 
 void printRussianAlphabetCodes() {
-    // Unicode ranges for Russian alphabet
-    const wchar_t lowercase_start = L'а'; // 'а'
-    const wchar_t lowercase_end = L'я';   // 'я'
-    const wchar_t uppercase_start = L'А'; // 'А'
-    const wchar_t uppercase_end = L'Я';   // 'Я'
+    
+    const wchar_t lowercase_start = L'а'; 
+    const wchar_t lowercase_end = L'я';   
+    const wchar_t uppercase_start = L'А'; 
+    const wchar_t uppercase_end = L'Я';   
 
-    // Print header
     std::wcout << std::setw(10) << L"Symbol"
                << std::setw(15) << L"Hex Code"
                << std::setw(15) << L"Octal Code" << std::endl;
     std::wcout << std::wstring(40, L'-') << std::endl;
 
-    // Print uppercase letters
     for (wchar_t c = uppercase_start; c <= uppercase_end; ++c) {
         std::wcout << std::setw(10) << c
                    << std::setw(15) << std::hex << std::showbase << (int)c
@@ -23,7 +21,6 @@ void printRussianAlphabetCodes() {
 
     std::wcout << std::endl;
 
-    // Print lowercase letters
     for (wchar_t c = lowercase_start; c <= lowercase_end; ++c) {
         std::wcout << std::setw(10) << c
                    << std::setw(15) << std::hex << std::showbase << (int)c
@@ -32,11 +29,10 @@ void printRussianAlphabetCodes() {
 }
 
 int main() {
-    // Set locale to support wide characters
+    
     std::locale::global(std::locale("ru_RU.utf8"));
     std::wcout.imbue(std::locale());
 
-    // Print the Russian alphabet codes
     printRussianAlphabetCodes();
 
     return 0;
